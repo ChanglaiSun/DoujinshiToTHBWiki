d20b555407c0b76929331a9f475650addf163f0c
xyqyear <215908558@qq.com>
2019-02-12 11:33:05 +0800

    optimise code

--- main.py
+++ main.py
@@ -1,108 +1,127 @@
 import requests
-import urllib
 from bs4 import BeautifulSoup
 import os
 import re
+
 
 def validateTitle(title):
     rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/\:*?"<>|'
     new_title = re.sub(rstr, "__", title)
     return new_title
 
-def getHTMLText(url,code="utf-8",timeout=30):
+
+def getHTMLText(url, code="utf-8", timeout=30):
     try:
-        r=requests.get(url)
+        r = requests.get(url, timeout=timeout)
         r.raise_for_status()
-        r.encoding=code
-        soup=BeautifulSoup(r.text,'html.parser')
+        r.encoding = code
+        soup = BeautifulSoup(r.text, 'lxml')
         return soup
-    except:
+    except Exception as e:
+        print('Error while getting html text: ', e)
         return ""
 
-def writeToFile(text,dirPath,filename):
-    file=open(dirPath+filename+'.txt','a')
-    file.write(text)
-    file.close()
+
+def writeToFile(text, dirPath, filename):
+    filePath = os.path.join(dirPath, filename+'.txt')
+    with open(filePath, 'a') as file:
+        file.write(text)
+
 
 def getBookInfo(url):
-    infoDict={}
-    circleList=''
-    staffList=[]
-    html=getHTMLText(url)
-    mainTable=html.find('table',attrs={'class':'mainborder'})
-    trList=mainTable.find_all('tr')
-    for i in range(1,12):
-        kv=trList[i].find_all('td')
-        key=kv[0].get_text()
-        value=kv[1].get_text()
-        infoDict[key]=value
-    point=mainTable.find('div',string='Circles:').find_parent().find_parent()
-    while True:
-        point=point.find_next_sibling()
-        if(point.find('div',string='Authors:')==None):
-            circleList=circleList+point.find('td').find_next_sibling().get_text()+'，'
-        else:
-            break
-    circleList=circleList[:-1]
-    point=mainTable.find('div',string='Authors:').find_parent().find_parent()
-    while True:
-        point=point.find_next_sibling()
-        if(point.find('div',string='Parodies:')==None):
-            staffList.append(point.find('td').find_next_sibling().get_text())
-        else:
-            break
-    cover=html.findAll('img',alt='cover')
-    if(cover!=None):
-        coverSrc=cover[0].get('src')
-    formResult(infoDict,circleList,staffList,coverSrc)
+    infoDict = {}
+    circleListStr = ''
+    staffList = []
+    html = getHTMLText(url)
+    mainTable = html.find('table', class_='mainborder')
+    trList = mainTable.find_all('tr')
+    for i in range(1, 12):
+        kv = trList[i].find_all('td')
+        key = kv[0].get_text()
+        value = kv[1].get_text()
+        infoDict[key] = value
+    circleListStr = ','.join(i.get_text() for i in mainTable.find_all('a', href=re.compile(r'circle')) if i.get_text())
+    staffList = [i.get_text() for i in mainTable.find_all('a', href=re.compile(r'author')) if i.get_text()]
+    cover = html.find('img', alt='cover')
+    coverSrc = cover['src'] if cover else ''
+    formResult(infoDict, circleListStr, staffList, coverSrc)
+
 
 def getCircleAll(url):
-    html=getHTMLText(url)
-    circleName=html.find('div',attrs={'id':'page_name'}).get_text().strip().replace('Circles: ','')
+    html = getHTMLText(url)
+    circleName = html.find('div', id='page_name').get_text().replace('Circles:', '').strip()
     global path
-    path=path+validateTitle(circleName)+'/'
+    path = os.path.join(path, validateTitle(circleName))
     if not os.path.exists(path):
         os.mkdir(path)
-    page=0
     while True:
-        page=page+1
-        html=getHTMLText(url+'?page='+str(page))
-        bookListHTML=html.find_all('div',attrs={'class':'bookinfo'})
-        if bookListHTML==[]:
-            break;
+        html = getHTMLText(url)
+        bookListHTML = html.find_all('div', class_='bookinfo')
+        if not bookListHTML:
+            break
         for each in bookListHTML:
-            if each.find(string='Touhou Project')!=None:
+            if each.find(string='Touhou Project'):
                 getBookInfo("https://www.doujinshi.org"+each.find_parent().find('a').get('href'))
 
-def formResult(infoDict,circleList,staffList,coverSrc=None):
-    result="{{同人志头部}}\n\n== 作品信息 ==\n{{同人志信息|\n| 封面 = {{PAGENAME}}封面.jpg\n| 封面角色 = \n| 名称 = [ORIGINALTITLE]\n| 译名 = \n| 制作方 = [CIRCLES]\n| 类型 = [TYPE]\n| 展会 = [CONVENTION]\n| 年龄限制 = [AGELIMIT]\n| 尺寸 = [SCALE]\n| 页数 = [PAGES]\n| 编号 = \n| 登场人物 = \n| 售价 = \n| 官网页面 = \n|\n}}\n\n== Staff ==\n[STAFF]\n== 评论 ==\n\n{{Bottom}}"
-    title=infoDict['Original title:']
-    result=result.replace('[ORIGINALTITLE]',title)
-    result=result.replace('[CIRCLES]',circleList)
-    result=result.replace('[TYPE]','漫画')
-    result=result.replace('[CONVENTION]',infoDict['Convention:'].strip())
-    if(infoDict['Adult:']=='Yes'):
-        result=result.replace('[AGELIMIT]','R18')
-    else:
-        result=result.replace('[AGELIMIT]','一般向')
-    result=result.replace('[SCALE]','B5')
-    result=result.replace('[PAGES]',infoDict['Pages:'])
-    staff=''
-    for eachStaff in staffList:
-        staff=staff+'*[['+eachStaff+']]\n'
-    result=result.replace('[STAFF]',staff)
-    writeToFile(result,path,infoDict['Released:']+'__'+validateTitle(title))
-    print(title)
-    saveCover(path,title,infoDict['Released:'],coverSrc)#comment out this line to disable save doujinshi cover
+        url = html.find(class_='next', string='►')
+        if url:
+            url = "https://www.doujinshi.org" + url['href']
+        else:
+            break
 
-def saveCover(path,title,releaseDate,coverSrc=None):
-    if(coverSrc!=None):
+
+def formResult(infoDict, circleListStr, staffList, coverSrc=None):
+    title = infoDict['Original title:']
+    staff = '\n'.join('*[['+eachStaff+']]' for eachStaff in staffList)
+    result = f'''{{{{同人志头部}}}}
+
+== 作品信息 ==
+{{{{同人志信息|
+| 封面 = {{{{PAGENAME}}}}封面.jpg
+| 封面角色 =
+| 名称 = {title}
+| 译名 =
+| 制作方 = {circleListStr}
+| 类型 = 漫画
+| 展会 = {infoDict['Convention:'].strip()}
+| 年龄限制 = {'R18' if infoDict['Adult:']=='Yes' else '一般向'}
+| 尺寸 = B5
+| 页数 = {infoDict['Pages:']}
+| 编号 =
+| 登场人物 =
+| 售价 =
+| 官网页面 =
+|
+}}}}
+
+== Staff ==
+{staff}
+
+== 评论 ==
+
+{{{{Bottom}}}}'''
+
+    fileTitle = validateTitle(title)
+    writeToFile(result, path, infoDict['Released:']+'__'+fileTitle)
+    print(title)
+    saveCover(path, fileTitle, infoDict['Released:'], coverSrc)  # comment out this line to disable save doujinshi cover
+
+
+def saveCover(path, title, releaseDate, coverSrc=None, timeout=30):
+    if coverSrc:
+        try:
+            imgContent = requests.get('https:' + coverSrc, timeout=timeout).content
+            if imgContent:
+                with open(os.path.join(path, f'{releaseDate}__{title}.jpg'), 'wb') as imgFile:
+                    imgFile.write(imgContent)
+        except Exception as e:
+            print('Error while saving cover: ', e)
         #print(coverSrc)
-        urllib.request.urlretrieve('https:'+coverSrc,"%s%s__%s封面.jpg"%(path,releaseDate,validateTitle(title)))
-        
-path='./'
+
+
+path=os.path.abspath('.')
 #getBookInfo("https://www.doujinshi.org/book/17221/")
 #getCircleAll("https://www.doujinshi.org/browse/circle/1974/")
 
-url=input("circle page 's url:")
+url = input("circle page 's url:")
 getCircleAll(url)
